2bb57a1fd5d4c381838397e9e3df5c0021d6c889
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-05-01 20:01:21 +0100

    Improved cooks index test

--- spec/user_views_cooks_spec.rb
+++ spec/user_views_cooks_spec.rb
@@ -2,11 +2,14 @@ require "rails_helper"
 
 feature "user views cooks" do
   scenario "successfully" do
+
+    Cook.create!(name: "TEST_NAME", cuisine: "TEST_CUISINE", city: "TEST_CITY")
+
     visit cooks_path
 
-    expect(page).to have_content "Cook"
-    expect(page).to have_content "Cuisine"
-    expect(page).to have_content "City"
+    expect(page).to have_content "TEST_NAME"
+    expect(page).to have_content "TEST_CUISINE"
+    expect(page).to have_content "TEST_CITY"
 
     expect(page).to have_link "Home"
     expect(page).to have_link "New cook"
