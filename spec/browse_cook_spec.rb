071ed0c17f398375be933147b130cd8f11c40d61
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-05-15 19:34:08 +0100

    removed edit test from cookfeature test

--- spec/browse_cook_spec.rb
+++ spec/browse_cook_spec.rb
@@ -15,23 +15,6 @@ feature "browse cook" do
     expect(page).to have_content "TEST_CITY"
   end
 
-  scenario "and edit successfully" do
-    cook = Cook.create!(
-      name: "TEST_NAME",
-      cuisine: "TEST_CUISINE",
-      city: "TEST_CITY",
-    )
-
-    visit cook_path(cook)
-
-    click_link "Edit"
-
-    fill_in "Name", with: "TEST_DIFFERENT_NAME"
-    click_button "Update Cook"
-
-    expect(page).to have_content "TEST_DIFFERENT_NAME"
-  end
-
   scenario "and destroy" do
     cook = Cook.create!(
       name: "TEST_NAME",
